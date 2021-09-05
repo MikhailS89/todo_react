bc24e286bc962e5dec93704db085f26ce7bdc34c
MikhailS89 <mihey2023@ya.ru>
2021-09-05 12:18:34 +0400

    Удалены лишние комментарии

--- src/componets/Todoitem.js
+++ src/componets/Todoitem.js
@@ -4,7 +4,6 @@ import './Todoitem.css';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Checkbox from '@material-ui/core/Checkbox';
-// import FormControl from '@material-ui/core/FormControl';
 
 
 export default function Todoitem({id, value, status}) {
@@ -14,13 +13,13 @@ export default function Todoitem({id, value, status}) {
         <div className="box__form">
             <form className="form">
                 <div className="form__checkbox">
-                    <Checkbox onChange={(event)=> changeChecked(id, event.target.checked)} defaultChecked={status} color="primary" inputProps={{ 'aria-label': 'uncontrolled-checkbox' }} />
+                <Checkbox onChange={(event)=> changeChecked(id, event.target.checked)} defaultChecked={status} color="primary" inputProps={{ 'aria-label': 'uncontrolled-checkbox' }} />
                 </div>
                 <div className="form__input">
-                    <TextField  onChange={(event)=> changeTodo (id, event.target.value)} defaultValue={value} />
+                <TextField  onChange={(event)=> changeTodo (id, event.target.value)} defaultValue={value} />
                 </div>
                 <div className="form__button">
-                    <Button onClick={()=> removeTodo(id)} variant="outlined">DEL</Button>
+                <Button onClick={()=> removeTodo(id)} variant="outlined">DEL</Button>
                 </div>
             </form>
         </div>
